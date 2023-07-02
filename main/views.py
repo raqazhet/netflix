299309d10130da0580f9b0ccc7e2c05744165a61
raqazhet <qazhetrazaq@gmail.com>
2023-07-02 17:42:39 +0600

    method post added

--- main/views.py
+++ main/views.py
@@ -5,10 +5,16 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,"index.html")
+    return render(request,"home.html")
 
 def postUser(request):
-    name = request.POST.get("name")  # Use 'POST' with uppercase letters
-    age = request.POST.get("age")
-    # Process the name data here
-    return HttpResponse("Hello, " + name + "!" + age)
+    if request.method == 'POST':  # Use 'method' with lowercase letters
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        # Process the name and age data here
+        return HttpResponse("Hello, " + name + "! You are " + age + " years old.")
+    elif request.method == 'GET':  # Use 'method' with lowercase letters
+        return render(request, "index.html")
+    else:
+        return HttpResponse("Method not allowed")
+

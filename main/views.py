50e06704da93957a2a617d194818f39ac15f1846
raqazhet <qazhetrazaq@gmail.com>
2023-07-02 19:55:15 +0600

    insert first user

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 
@@ -9,10 +10,12 @@ def home(request):
 
 def postUser(request):
     if request.method == 'POST':  # Use 'method' with lowercase letters
-        name = request.POST.get("name")
-        age = request.POST.get("age")
+        user =User()
+        user.name = request.POST.get("name")
+        user.age = request.POST.get("age")
+        user.save()
         # Process the name and age data here
-        return HttpResponse("Hello, " + name + "! You are " + age + " years old.")
+        return HttpResponse("Hello, " + user.name + "! You are " + user.age + " years old.")
     elif request.method == 'GET':  # Use 'method' with lowercase letters
         return render(request, "index.html")
     else:

f9c0341e70ed820051d12783688e572f05ab7eb5
raqazhet <qazhetrazaq@ghmail.com>
2023-07-03 09:59:41 +0600

    user age error

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 from .models import User
 
 # Create your views here.
@@ -12,10 +13,15 @@ def postUser(request):
     if request.method == 'POST':  # Use 'method' with lowercase letters
         user =User()
         user.name = request.POST.get("name")
-        user.age = request.POST.get("age")
+        try:
+            user.age = int(request.POST.get("age"))
+            if user.age <10 or user.age>100:
+                return HttpResponseBadRequest("invalid data")
+        except ValueError:
+            return HttpResponseBadRequest("Invalid age value")
         user.save()
         # Process the name and age data here
-        return HttpResponse("Hello, " + user.name + "! You are " + user.age + " years old.")
+        return HttpResponse("Hello, " + user.name + "! You are " + str(user.age) + " years old.")
     elif request.method == 'GET':  # Use 'method' with lowercase letters
         return render(request, "index.html")
     else:
